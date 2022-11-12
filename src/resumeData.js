7c1968d1ac361cb17190f10d9c400f7ecd76f7a1
swikriti04 <swikritikhadke@gmail.com>
2022-11-13 02:37:52 +0530

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,145 @@
+let resumeData = {
+    "imagebaseurl":"https://swikriti04.github.io/Mywebsite/",
+    "name": "Swikriti Khadke",
+    "role": "Full Stack Developer and a Data Scientist",
+    "linkedinId":"https://www.linkedin.com/in/swikriti-khadke-2a7468193/",
+    "skypeid": "Your skypeid",
+    "roleDescription": " I am an IOT enthusiast and an AI researcher. I have keen interest in full stack development, AR/VR and I'm well versed with computer vision. I have worked on multidisciplinary projects. I love to tackle real life challenges and innovate something interesting. I'm fascinated with the new technologies and always want to contribute my good part to the society.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/swikriti-khadke-2a7468193/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/swikriti04",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"twitter",
+          "url":"https://twitter.com/Musiclusive1",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am Computer Science Engineer specialized in Artificial Intelligence and Machine Learning. I did my Engineering from SRM University, AP, India. I am a quick learner and I am always keen to learn something new. I keep learning new technologies and implement them in my projects in order to solve a real life problem.",
+    "address":"Canada",
+    "website":"https://swikriti04.github.io/Mywebsite/",
+    "education":[
+      {
+        "UniversityName":"SRM University, AP",
+        "specialization":"B.Tech in Computer Science Engineering",
+        "MonthOfPassing":"July 2019 - ",
+        "YearOfPassing":"May 2023",
+        "Achievements":"I secured gold medal on Research Day on 'Solar Power Prediction' organized by SRM University."
+      },
+      {
+        "UniversityName":"St. Joseph's Convent School",
+        "specialization":"Higher Secondary School",
+        "MonthOfPassing":"2017 - ",
+        "YearOfPassing":"2019",
+        "Achievements":"I scored 92% in Science Stream (PCM)"
+      },
+      {
+        "UniversityName":"St. Peters School",
+        "specialization":"Secondary School",
+        "MonthOfPassing":"2016 - ",
+        "YearOfPassing":"2017",
+        "Achievements":"I scored 9.8 CGPA"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Volante Systems - Toronto, Canada",
+        "specialization":"Product Specialist",
+        "MonthOfLeaving":"Sep 2022 - ",
+        "YearOfLeaving":"Present",
+        "Achievements":""
+      },
+      {
+        "CompanyName":"Université Du Québec Research Lab - Quebec, Canada",
+        "specialization":"Mitacs Research Intern",
+        "MonthOfLeaving":"June 2022 - ",
+        "YearOfLeaving":"Aug 2022",
+        "Achievements":""
+      },
+      {
+        "CompanyName":"Sunkonnect Pte. ltd - Singapore",
+        "specialization":"Android App Developer",
+        "MonthOfLeaving":"Oct 2021 - ",
+        "YearOfLeaving":"Nov 2021",
+        "Achievements":""
+      },
+      {
+        "CompanyName":"Sunkonnect Pte. ltd - Singapore",
+        "specialization":"Machine Learning Intern",
+        "MonthOfLeaving":"June 2021 - ",
+        "YearOfLeaving":"Sep 2021",
+        "Achievements":""
+      }
+    ],
+    "skillsDescription":"My Skills are",
+    "skills":[
+      {
+        "skillname":"C/C++"
+      },
+      {
+        "skillname":"Python"
+      },
+      {
+        "skillname":"React Native, Js"
+      },
+      {
+        "skillname":"HTML, CSS"
+      },
+      {
+        "skillname":"AR/VR"
+      },
+      {
+        "skillname":"SQL"
+      },
+      {
+        "skillname":"AI/ML"
+      },
+      {
+        "skillname":"Web Development, Designing"
+      },
+      {
+        "skillname":"Public Speaking, Leadership"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Project1",
+        "description":"Covid-19 Tracker - A smart cane for visually impaired people. This cane will give a real view of outside world to these people by detecting what’s around them.",
+        "imgurl":"images/covid.png"
+      },
+      {
+        "name":"Project2",
+        "description":"Airline Management System - An automated system for airline passengers.",
+        "imgurl":"images/airline.png"
+      },
+      {
+        "name":"Project3",
+        "description":"Smart cane - This React application fetches live data from API and displays number of cases of Covid-19",  
+        "imgurl":"images/cane.jpg"
+      },
+      {
+        "name":"Project4",
+        "description":"GetSunSolar App - An App made for solar providers to inspect customer's site.",
+        "imgurl":"images/sunkonnect.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"She is very hardworking and determined.",
+        "name":"Gautam Kumar Dalapati"
+      },
+      {
+        "description":"You are a great and talented person and I'm really happy that you worked with us.",
+        "name":"Behzad Hashemi"
+      }
+    ]
+  }
+  
+  export default resumeData
