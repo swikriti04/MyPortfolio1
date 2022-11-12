273a679346ce459f090514712475463b951dbcbb
swikriti04 <swikritikhadke@gmail.com>
2022-11-13 03:04:09 +0530

    Updates

---
+++ precache-manifest.dc7066879062702232f0888185b47b8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e387abe9428dea035a92",
+    "url": "/swikriti04/MyPortfolio1/static/css/main.b00cb5dc.chunk.css"
+  },
+  {
+    "revision": "e387abe9428dea035a92",
+    "url": "/swikriti04/MyPortfolio1/static/js/main.e387abe9.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/swikriti04/MyPortfolio1/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "2a2f76454e18b8632e9f",
+    "url": "/swikriti04/MyPortfolio1/static/js/runtime~main.2a2f7645.js"
+  },
+  {
+    "revision": "0723734ebcdc06734f5f73828e3cb7f2",
+    "url": "/swikriti04/MyPortfolio1/index.html"
+  }
+];
